ac3e618fd219aeedbedf4ec7b6bae72121e138ae
Jordan Kasper <jordan@jordankasper.com>
2024-12-16 16:03:32 -0500

    Day16 part 2

--- lib/Day16.php
+++ lib/Day16.php
@@ -21,26 +21,26 @@ class Day16
         foreach ($map as $y => $row) {
             foreach ($row as $x => $spot) {
                 if ($spot == "S") {
-                    $start = "$y,$x-1";
+                    $start = "$y-$x-1";
                 }
                 if ($spot == "E") {
-                    $ends = ["$y,$x-0", "$y,$x-1", "$y,$x-2", "$y,$x-3"];
+                    $ends = ["$y-$x-0", "$y-$x-1", "$y-$x-2", "$y-$x-3"];
                 }
                 if ($spot != "#") {
                     foreach ($dirs as $i => $m) {
-                        $graph["$y,$x-$i"] = [];
+                        $graph["$y-$x-$i"] = [];
                         // moves
                         $my = $y + $m[0];
                         $mx = $x + $m[1];
                         if ($map[$my][$mx] != "#") {
-                            $graph["$y,$x-$i"]["$my,$mx-$i"] = 1;
+                            $graph["$y-$x-$i"]["$my-$mx-$i"] = 1;
                         }
 
                         // pivots
                         $left = (($i-1) < 0) ? 3 : $i-1;
                         $right = (($i+1) > 3) ? 0 : $i+1;
-                        $graph["$y,$x-$i"]["$y,$x-$left"] = 1000;
-                        $graph["$y,$x-$i"]["$y,$x-$right"] = 1000;
+                        $graph["$y-$x-$i"]["$y-$x-$left"] = 1000;
+                        $graph["$y-$x-$i"]["$y-$x-$right"] = 1000;
                     }
                 }
             }
@@ -49,23 +49,55 @@ class Day16
         // Day16::show_map($map);
         // Logger::log(json_encode($graph["13,1-0"]));
 
-        $distances = Day16::calc_distances($graph, $start);
+        $results = Day16::calc_distances($graph, $start);
+        $distances = $results[0];
+        $paths = $results[1];
+
         $p1 = 9999999999;
         foreach ($ends as $i => $end) {
-            Logger::log("dist to $end: {$distances[$end]}");
+            // Logger::log("dist to $end: {$distances[$end]}");
             if ($distances[$end] < $p1) { $p1 = $distances[$end]; }
         }
         Logger::log("Part 1: $p1");
 
-        // Logger::log("Part 2: $p2");
+        $seats = [];
+        foreach ($ends as $i => $end) {
+            if ($distances[$end] == $p1) {
+                // Logger::log("finding path to $end ({$distances[$end]})");
+
+                $q = [$end];
+                while (count($q) > 0) {
+                    $next = array_pop($q);
+                    $spot = preg_split('/\-/', $next);
+                    if (!isset($seats["{$spot[0]}-{$spot[1]}"])) {
+                        $seats["{$spot[0]}-{$spot[1]}"] = 1;
+                    }
+                    // Logger::log("access $next in paths:");
+                    // Logger::log(json_encode($paths[$next]));
+                    foreach ($paths[$next] as $i => $node) {
+                        if ($node != $start) {
+                            array_push($q, $node);
+                        }
+                    }
+                }
+                $s = preg_split('/\-/', $start);
+                $seats["{$s[0]}-{$s[1]}"] = 1;
+            }
+        }
+        // Logger::log(json_encode($seats));
+
+        $p2 = count($seats);
+        Logger::log("Part 2: $p2");
     }
 
     private static function calc_distances($graph, $start) {
         $max = 999999999;
         $distances = [];
+        $paths = [];
         
         foreach ($graph as $node => $moves) {
             $distances[$node] = $max;
+            $paths[$node] = [];
         }
         $distances[$start] = 0;
 
@@ -81,11 +113,14 @@ class Day16
                 $dist = $curr[0] + $weight;
                 if ($dist < $distances[$node]) {
                     $distances[$node] = $dist;
+                    $paths[$node] = [$curr[1]];
                     array_push($q, [$dist, $node]);
+                } else if ($dist == $distances[$node]) {
+                    array_push($paths[$node], $curr[1]);
                 }
             }
         }
-        return $distances;
+        return [$distances, $paths];
     }
 
     private static function show_map($map) {
